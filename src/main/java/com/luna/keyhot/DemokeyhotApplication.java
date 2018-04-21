a925984bff9e8afa37c9f236e9bc12496456e8ed
xiaolige <mycontroldestiny@126.com>
2018-04-21 19:13:02 +0800

    init

---
+++ src/main/java/com/luna/keyhot/DemokeyhotApplication.java
@@ -0,0 +1,15 @@
+package com.luna.keyhot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemokeyhotApplication {
+
+	public static void main(String[] args) {
+
+
+
+		SpringApplication.run(DemokeyhotApplication.class, args);
+	}
+}
